e8d4d2119967046f395b698d7b8d1e18e0abf2af
itspacchu <37984032+itspacchu@users.noreply.github.com>
2023-01-06 13:16:27 +0530

    corrupted import files fix

--- Scripts/sinescaler.gd
+++ Scripts/sinescaler.gd
@@ -13,13 +13,18 @@ export var big_ol_list_of_motd = [
 	"Metamorphosis",
 	"Made on ArchLinux btw!",
 	"Wednesday Biryani",
-	"prashantnook.in",
-	"Hyderabad edition"
+	"Hyderabad edition",
+	"Stonks!",
+	"yeeeeeeeet",
+	"Wildest Dreams",
+	"Giggity Giggity",
+	"Lateraus ~ Tool",
+	"The Nights"
 ]
 
 func _ready():
-	#$Label.text = big_ol_list_of_motd[rand_range(0,len(big_ol_list_of_motd))]
-	$Label.text = big_ol_list_of_motd[len(big_ol_list_of_motd)-1]
+	$Label.text = big_ol_list_of_motd[rand_range(0,len(big_ol_list_of_motd))]
+	#$Label.text = big_ol_list_of_motd[len(big_ol_list_of_motd)-1]
 
 func _process(delta):
 	rect_scale = Vector2.ONE * (scale_max * (sin(periodic*OS.get_ticks_msec()/100)+1)*0.5 + dc_scale)
