b8d29af33574cc3065516cfd78c94e8cbf25dcac
itspacchu <37984032+itspacchu@users.noreply.github.com>
2023-01-20 02:13:00 +0530

    camera should shift

--- Scripts/syncmapcamtoglobal.gd
+++ Scripts/syncmapcamtoglobal.gd
@@ -4,5 +4,5 @@ export (NodePath) var target;
 onready var player = get_node(target)
 
 func _physics_process(delta):
-	$Viewport/mapcam.translation = Vector3(player.translation.x,30,player.translation.y)
+	$Viewport/mapcam.translation = Vector3(player.translation.x,30,player.translation.y) + Vector3(0,0,-10)
 	$Viewport/Sprite3D.translation = Vector3(player.translation.x,30,player.translation.y)
