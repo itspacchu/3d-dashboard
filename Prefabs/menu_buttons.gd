b1a6e335708e1d392b91e9108139d5ae5144c0b7
itspacchu <37984032+itspacchu@users.noreply.github.com>
2023-07-14 13:36:22 +0530

    fix build bugs

--- Prefabs/menu_buttons.gd
+++ Prefabs/menu_buttons.gd
@@ -7,12 +7,10 @@ func _input(event: InputEvent) -> void:
 func _on_Button_button_down():
 	$Control/instruction.visible = true
 	var t = Timer.new()
-	t.set_wait_time(2)
+	t.set_wait_time(0.5)
 	t.set_one_shot(true)
 	self.add_child(t)
 	t.start()
 	yield(t, "timeout")
 	t.queue_free()
 	get_tree().change_scene("res://Scenes/TestBed.tscn")
-	
-	
